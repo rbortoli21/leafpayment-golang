2735a1fcc9b997529317fc5523ad0a2954bcd312
Rafael <rafaelbortoli21@gmail.com>
2024-04-17 22:28:32 -0300

    disable cors

--- cmd/main.go
+++ cmd/main.go
@@ -14,5 +14,21 @@ func main() {
 	http.HandleFunc("/generate-payroll/{id}", handler.GeneratePayrollHandler)
 	http.HandleFunc("/find/{id}", handler.GetEmployerById)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	handler := corsMiddleware(http.DefaultServeMux)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
